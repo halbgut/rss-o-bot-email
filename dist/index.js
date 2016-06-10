1bdcf97db787b090af0198ad77bbc8bdfae51c10
Kriegslustig <kriegslustig@kriegslustig.me>
2016-06-10 18:11:22 +0200

    Run build

--- dist/index.js
+++ dist/index.js
@@ -9,11 +9,11 @@ module.exports = function email(config) {
   var transporter = nodemailer.createTransport(config['email-configuration'] || { direct: true, debug: true, logger: true });
   var sendmail = Rx.Observable.fromNodeCallback(transporter.sendMail.bind(transporter));
   var to = config['email-recipients'].join(', ');
-  return function (subject, text) {
+  return function (subject, url, title) {
     return debug('Sending email to ' + to) || sendmail({
-      from: 'RSS-o-Bot <' + os.userInfo().username + '@' + os.hostname() + '>',
-      html: '<a href="' + text + '">text</a>',
-      subject: subject, text: text, to: to
+      from: config['email-from'] || 'RSS-o-Bot <' + os.userInfo().username + '@' + os.hostname() + '>',
+      html: '<a href="' + url + '">' + title + '</a>',
+      subject: subject, text: title + ': ' + url, to: to
     });
   };
 };
